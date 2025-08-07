cff617227d256c8a52cf0595641a6f803c6130a3
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-07 16:42:24 +0800

    Add: headings and svg animations

--- src/main/main.js
+++ src/main/main.js
@@ -1,1 +1,13 @@
 import './display-pic.js';
+import { addAnimation } from '../utils/animation-utils.js';
+
+// tilt and fade animation
+(function () {
+  const heading = document.querySelector('.educ');
+  const svg = heading.querySelector('.grad-cap');
+  const anchor = heading.querySelector('.anchor');
+  const tiltAnimation = 'tilt';
+  const fadeAnimation = 'fade';
+  addAnimation(heading, tiltAnimation, svg);
+  addAnimation(heading, fadeAnimation, anchor);
+})();
