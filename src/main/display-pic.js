4212bb3a8eb4181538d3b48dd5aa0688ada8dce8
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-07 16:37:44 +0800

    Modify: import util fn and refactor animation fn

--- src/main/display-pic.js
+++ src/main/display-pic.js
@@ -1,14 +1,8 @@
-// rotate animation
-
+import { addAnimation } from "../utils/animation-utils.js"
+;
+// rotate display-pic pseudo-elements
 (function () {
   const picWrapper = document.querySelector('.pic-wrapper');
-  picWrapper.addEventListener('click', rotateAnimate);
-
-  function rotateAnimate () {
-    picWrapper.classList.add('rotate');
-
-    picWrapper.addEventListener('animationend', () => {
-      picWrapper.classList.remove('rotate');
-    })
-  }
+  const animationName = 'rotate';
+  addAnimation(picWrapper, animationName);
 })();
