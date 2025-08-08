b881af36922b4383b461fb8c1c5c6e0bd790ccef
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-08 14:57:28 +0800

    Modify: removed animation

--- src/main/display-pic.js
+++
@@ -1,8 +0,0 @@
-import { addAnimation } from "../utils/animation-utils.js"
-;
-// rotate display-pic pseudo-elements
-(function () {
-  const picWrapper = document.querySelector('.pic-wrapper');
-  const animationName = 'rotate';
-  addAnimation(picWrapper, animationName);
-})();
