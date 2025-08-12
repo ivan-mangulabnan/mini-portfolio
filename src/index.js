123e6d39d8df671fcf6d0e4bfe54a178e3edce76
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-13 00:47:02 +0800

    Add: educ animations, anchor and footer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './main.css';
 import './banner/banner.js';
+
+const educh2 = document.querySelector('.educ h2');
+educh2.addEventListener('mouseenter', () => {
+  const svg = educh2.querySelector('svg');
+  const anchor = educh2.querySelector('.anchor');
+
+  svg.classList.add('move-cap');
+  anchor.classList.add('show-anchor');
+
+  anchor.addEventListener('animationend', () => {
+    anchor.classList.remove('show-anchor');
+  })
+
+  svg.addEventListener('animationend', () => {
+    svg.classList.remove('move-cap');
+  })
+})
