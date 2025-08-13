8d3b9d6f9209968b10aeb3e5b8748bc1f62b2768
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-13 14:31:12 +0800

    Feat: attack animation on home

--- src/banner/logo/logo.js
+++ src/banner/logo/logo.js
@@ -1,20 +1,22 @@
 // svg hand-pop animations
 
-(function () {
-  const logoDiv = document.querySelector('.banner .logo');
+const logoDiv = document.querySelector('.banner .logo');
+const home = document.querySelector('.base');
+const svg = document.querySelector('.logo-svg');
 
-  let running = false;
-  logoDiv.addEventListener('mouseenter', () => {
-    if (running) return;
-    const svg = document.querySelector('.logo-svg');
-    svg.classList.add('hand-pop');
-    running = true;
+let running = false;
 
-    svg.addEventListener('animationend', (event) => {
-      if (event.animationName === 'hand-hide') {
-        svg.classList.remove('hand-pop');
-        running = false;
-      }
-    })
-  })
-})();
+logoDiv.addEventListener('mouseenter', showLike);
+home.addEventListener('click', showLike);
+
+function showLike () {
+  if (running) return;
+  svg.classList.add('hand-pop');
+  running = true;
+}
+
+svg.addEventListener('animationend', (event) => {
+  if (event.animationName !== 'hand-hide') return;
+  svg.classList.remove('hand-pop');
+  running = false;
+})
