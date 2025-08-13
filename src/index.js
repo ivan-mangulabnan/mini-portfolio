39922ca98d7b91e29713ae3392bc4326c30b9e29
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-08-13 10:31:41 +0800

    Feat: add animation upon link click and on element hover

--- src/index.js
+++ src/index.js
@@ -2,18 +2,53 @@ import './main.css';
 import './banner/banner.js';
 
 const educh2 = document.querySelector('.educ h2');
-educh2.addEventListener('mouseenter', () => {
-  const svg = educh2.querySelector('svg');
-  const anchor = educh2.querySelector('.anchor');
+const educLink = document.querySelector('[href="#educ"]');
+const educSvgClassName = 'move-cap';
+animate(educh2, educSvgClassName, educLink);
 
-  svg.classList.add('move-cap');
-  anchor.classList.add('show-anchor');
+// utils below}
 
-  anchor.addEventListener('animationend', () => {
-    anchor.classList.remove('show-anchor');
-  })
+function animate(header, svgClassName, clickEle) {
+  const state = {
+    anchorStillAnimating: false,
+    svgStillAnimating: false
+  };
 
-  svg.addEventListener('animationend', () => {
-    svg.classList.remove('move-cap');
-  })
-})
+  const handleHover = () => onHeaderHover(header, svgClassName, state);
+  header.addEventListener('mouseenter', handleHover);
+  clickEle.addEventListener('click', handleHover);
+}
+
+function onHeaderHover (header, svgClassName, state) {
+  if (state.anchorStillAnimating && state.svgStillAnimating) return;
+
+  state.svgStillAnimating = true;
+  state.anchorStillAnimating = true;
+
+  const svg = header.querySelector('svg');
+  const anchor = header.querySelector('.anchor');
+  const anchorClassName = 'show-anchor';
+
+  const svgEnd = () => onSVGEnd(svg, svgClassName, state);
+  const anchorEnd = () => onAnchorEnd(anchor, anchorClassName, state);
+
+  if (state.svgStillAnimating) {
+    svg.classList.add(svgClassName);
+    svg.addEventListener('animationend', svgEnd, {once: true});
+  }
+
+  if (state.anchorStillAnimating) {
+    anchor.classList.add(anchorClassName);
+    anchor.addEventListener('animationend', anchorEnd, {once: true});
+  }
+}
+
+function onSVGEnd (ele, className, state) {
+  ele.classList.remove(className);
+  state.svgStillAnimating = false;
+}
+
+function onAnchorEnd (ele, className, state) {
+  ele.classList.remove(className);
+  state.anchorStillAnimating = false;
+}
